3a140192024519f8bb38cc2393cf87f244d4bce1
fod-bello <20111965@setu.ie>
2026-09-25 15:46:14 +0100

    Mark task as done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -5,7 +5,8 @@ const Task = (props) => {
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
-            <p className="priority"> {props.priority}</p>
+            <p className="priority">Priority: {props.priority}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
     )
 }
